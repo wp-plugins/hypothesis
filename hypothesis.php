4cc411e55f079d0b6cc229b501eb21a81a3719ef
BigBlueHat <BigBlueHat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-30 17:26:51 +0000

    Preparing release 0.1.1.
    git-svn-id: https://plugins.svn.wordpress.org/hypothesis/trunk@1123783 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hypothesis.php
+++ hypothesis.php
@@ -1,15 +1,15 @@
 <?php
 /**
  * @package Hypothesis
- * @version 0.1
+ * @version 0.1.1
  */
 /*
 Plugin Name: Hypothesis
 Plugin URI: http://hypothes.is/
 Description: Hypothesis is an open platform for the collaborative evaluation of knowledge. This plugin embeds the necessary scripts in your Wordpress site to enable any user to use Hypothesis without installing any extensions.
-Author: Tim Owens
-Version: 0.1
-Author URI: http://timmmmyboy.com/
+Author: The Hypothesis Project and contributors
+Version: 0.1.1
+Author URI: http://hypothes.is/
 */
 
 // Exit if called directly
@@ -20,7 +20,7 @@ defined( 'ABSPATH' ) or die ( 'Cannot access pages directly.' );
  */
 function add_hypothesis() {
 	if ( !is_admin() ) 
-        wp_enqueue_script( 'hypothesis', 'https://hypothes.is/app/embed.js', '', false, true );
+        wp_enqueue_script( 'hypothesis', '//hypothes.is/embed.js', '', false, true );
 }
 
 add_action( 'init', 'add_hypothesis' );
